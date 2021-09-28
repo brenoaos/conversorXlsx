815f7d255fad2c8815cff1aa84175a30adf5730a
Breno Oliveira <breno.oliveira@avancoinfo.com.br>
2021-09-27 22:42:15 -0300

    Final

--- app/render.js
+++ app/render.js
@@ -1,22 +1,39 @@
 'use strict';
-const { readFileSync, existsSync, writeFileSync, unlinkSync, appendFileSync } = require('fs');
+const { readFileSync, existsSync, writeFileSync, unlinkSync, appendFileSync, readdirSync } = require('fs');
 const { randomBytes } = require('crypto');
 const { join, normalize } = require('path')
 const excelToJson = require('convert-excel-to-json');
+const { unzipSync } = require('zlib');
 
 var btnCarregar = document.getElementById("btnCarregar");
-var btnDepurar = document.getElementById("btnDepurar");
+var btnDepurar = document.getElementById("btnDepurarArea");
 var labelSystem = document.getElementById("label");
 var iptArquivo = document.getElementById('iptArquivo');
 var fileTmp = 'tmp-' + randomBytes(5).toString('hex');
 var divChaves = document.getElementById("chaves");
 var loader = document.getElementById("loader");
-var downloads = document.getElementById("downloads")
+var downloads = document.getElementById("downloadsArea")
 
 var arquivo = '';
 var chaves = [];
 var chavesSelecionadas = [];
 var chavesLidas = [];
+
+var chavePadrao = ['LOJA', 'DATA_MOVIMENTO', 'CODIGO', 'NF_CUPOM', 'VALOR', 'CNPJ']
+
+function init() {
+
+  let arquivos = readdirSync(__dirname)
+  arquivos.forEach(arq => {
+    if (arq.startsWith('tmp-')) {
+      unlinkSync(arq)
+      console.log('Excluindo ' + arq);
+    }
+  })
+
+  downloads.style.display = 'none';
+  btnDepurar.style.display = 'none';
+}
 
 function depurarArquivo() {
 
@@ -27,14 +44,14 @@ function depurarArquivo() {
     return
   }
 
-  alert("Idenficando chaves selecionadas..");
+  alert("Idenficando chaves selecionadas...");
 
   for (let elem of divChaves.children) {
     if (elem.children[1].checked) chavesSelecionadas.push(elem.children[1].id.toUpperCase())
   }
 
   if (chavesSelecionadas.length === 0) {
-    alert('Nehuma coluna selecionada');
+    alert('Nehuma coluna selecionada.');
     return;
   }
 
@@ -44,12 +61,18 @@ function depurarArquivo() {
   let cabecalho = { originais: false, duplicados: false }
   let result = JSON.parse(readFileSync(fileTmp, 'utf-8'));
 
-  alert("Depurando o arquivo");
+  alert("Depurando o arquivo.\nPor favor, aguarde!!");
 
   for (let dado of result) {
     key = "";
     for (let chave of chavesSelecionadas) {
-      key += dado[chave].trim()
+      try {
+        key += dado[chave].trim()
+      } catch (e) {
+        console.log('attr => ' + chave)
+        console.log('obj => ' + JSON.stringify(dado))
+        console.log(e.toString())
+      }
     }
 
     if (chavesLidas.indexOf(key) > -1) {
@@ -78,7 +101,8 @@ function depurarArquivo() {
   }
 
   unlinkSync(fileTmp);
-  alert("Depuração Concluída..");
+  alert("Depuração concluída com sucesso!!!");
+  downloads.style = 'block';
 }
 
 function carregarArquivo() {
@@ -92,7 +116,9 @@ function carregarArquivo() {
     for (let ch of divChaves.children) divChaves.removeChild(ch)
   }
 
-  alert("Carregando arquivo de entrada..");
+  alert("Carregando arquivo de entrada.\nPor favor, aguarde!");
+
+  init();
 
   arquivo = iptArquivo.files[0].path;
 
@@ -102,36 +128,40 @@ function carregarArquivo() {
 
   chaves = result.shift();
 
-  var novoJson = {}
+  var novoJson = []
 
   for (let row of result) {
-
+    let obj = {}
     for (let attr in row) {
 
-      novoJson[chaves[attr]] = row[attr]
+      obj[chaves[attr.toString().trim()]] = row[attr].toString().trim()
 
     }
+
+    novoJson.push(obj)
 
   }
 
   writeFileSync(fileTmp, JSON.stringify(novoJson))
 
-
   for (let chave of Object.values(chaves)) {
-    if (!chave) continue;
+    chave = chave.trim();
+    if (!chave.trim()) continue;
     var label = document.createElement('label')
     var check = document.createElement('input')
     var div = document.createElement('div')
 
     check.type = 'checkbox';
     check.id = chave.toLowerCase();
-    check.classList = ['form-check-input'];
+    check.className = "form-check-input";
+
+    if (chavePadrao.indexOf(chave) > -1) check.checked = true;
 
     label.innerHTML = chave;
-    label.classList = ['form-check-label'];
+    label.className = "form-check-label";
     label.htmlFor = chave.toLowerCase();
 
-    div.classList = ['form-check'];
+    div.className = "form-check form-switch col-6 text-start";
     div.appendChild(label);
     div.appendChild(check);
 
@@ -139,8 +169,8 @@ function carregarArquivo() {
 
   }
 
-  alert("Arquivo de entrada Carregado com sucesso.\nSelecione os atributos.");
-
+  alert("Arquivo de entrada carregado com sucesso.\nSelecione os atributos.");
+  btnDepurar.style = 'block';
 }
 
 const escreveCsv = (file, obj, cabecalho = false) => {
